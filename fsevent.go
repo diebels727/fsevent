cddad32715f689d4bf58d1d791903f9c2da65f80
Jonathan Quigg <jonathan.quigg@gd-ais.com>
2015-01-08 14:31:40 -0500

    Execute command on close.

--- fsevent.go
+++ fsevent.go
@@ -4,20 +4,23 @@ import (
 	"flag"
 	"github.com/diebels727/fsnotify"
 	"log"
+  "os/exec"
 )
 
 var watch string
-var cmd string
+var command string
+var commandMessage string
 
 func init() {
 	flag.StringVar(&watch, "watch", "/tmp", "path or file to watch for changes")
-	flag.StringVar(&cmd, "cmd", "", "OS command to execute")
+	flag.StringVar(&command, "command", "", "OS command to execute; should be full path")
+  flag.StringVar(&commandMessage,"message","Executing.","Message to display when this command executes.")
 }
 
 func main() {
 	flag.Parse()
 	log.Printf("Watching %s", watch)
-	log.Printf("  Command \"%s\" will execute on file change operations.", cmd)
+	log.Printf("Command \"%s\" will execute on file change operations.", command)
 	watcher, err := fsnotify.NewWatcher()
 	if err != nil {
 		log.Fatal(err)
@@ -30,9 +33,13 @@ func main() {
 			select {
 			case event := <-watcher.Events:
 				log.Println("event:", event)
-
 				if event.Op&fsnotify.Close == fsnotify.Close {
-					log.Println("Will fire!")
+          log.Println(commandMessage)
+          cmd := exec.Command(command)
+          err := cmd.Run()
+          if err != nil {
+            log.Println(err)
+          }
 				}
 			case err := <-watcher.Errors:
 				log.Println("error:", err)
